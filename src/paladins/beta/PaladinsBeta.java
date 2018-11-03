c16e58510a3f2c8cb30078281b4e8949ce13ade5
Panagiotis Christoforakis <panaris666paparis@windowslive.com>
2018-11-03 02:24:32 +0200

    Update src/paladins/beta/PaladinsBeta.java

--- src/paladins/beta/PaladinsBeta.java
+++ src/paladins/beta/PaladinsBeta.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class PaladinsBeta {
 
-    public static void main(String[] args) {
-        
+    public static void main (String[] args) {
+        System.out.println ("Welcome");
     }
 }
