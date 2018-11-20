2e82f3fa862bb225ae480d7f9cd42cc6eb9b1079
Mike13 <44746063+MikeTs13@users.noreply.github.com>
2018-11-20 23:56:41 +0200

    "new class WindowsBoxes == intro window"

--- src/paladins/beta/PaladinsBeta.java
+++ src/paladins/beta/PaladinsBeta.java
@@ -6,5 +6,7 @@ public class PaladinsBeta {
 
     public static void main (String[] args) {
         System.out.println ("Welcome");
+        WindowsBoxes wb1 = new WindowsBoxes();       
+        wb1.setVisible(true);
     }
 }
